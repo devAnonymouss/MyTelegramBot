fba1f94e1855f853f3b6b9dc6df87119f246fe38
devAnonymouss <developer13267@gmail.com>
2022-12-20 21:47:05 +0300

    Add file

--- create_bot.py
+++ create_bot.py
@@ -1,9 +1,9 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-import os
+from config import TOKEN
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
 
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=storage)
